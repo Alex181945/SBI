af1309410c15ce8e7b04caa55aab9f0927befb8a
Alex181945 <alestra55@hotmail.com>
2018-02-20 23:24:10 -0600

    Inicio de Proyecto

---
+++ sbi/src/main/java/com/senado/sbi/SbiApplication.java
@@ -0,0 +1,12 @@
+package com.senado.sbi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbiApplication.class, args);
+	}
+}
