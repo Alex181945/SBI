27c3d8baf09597caabfdecf572eaa3414f467485
Alex181945 <alestra55@hotmail.com>
2018-04-10 17:01:44 -0500

    Sesion al controlador de tickets

--- sbi/src/main/java/com/senado/sbi/controlador/ticket/Ticket.java
+++ sbi/src/main/java/com/senado/sbi/controlador/ticket/Ticket.java
@@ -2,17 +2,19 @@ package com.senado.sbi.controlador.ticket;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.senado.sbi.configuracion.Vistas;
+import com.senado.sbi.modelo.seg.login.ULogin;
 
 @Controller
 @SessionAttributes("Usuario")
 public class Ticket {
 	
 	@GetMapping("/ticket")
-	public ModelAndView Inicio() {
+	public ModelAndView Inicio(@ModelAttribute("Usuario") ULogin sessionUsu) {
 		ModelAndView mav = new ModelAndView();
 		mav.setViewName(Vistas.getTicket());
 		mav.addObject("titulo", "Bienvenido");
