ef855994cb11f87d1adb4506deb431ee4f042145
Alex181945 <alestra55@hotmail.com>
2018-04-20 02:07:53 -0500

    Se elimina import

--- sbi/src/main/java/com/senado/sbi/controlador/seg/login/LDAP.java
+++ sbi/src/main/java/com/senado/sbi/controlador/seg/login/LDAP.java
@@ -2,7 +2,6 @@ package com.senado.sbi.controlador.seg.login;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 
 import com.senado.sbi.configuracion.Vistas;
 
