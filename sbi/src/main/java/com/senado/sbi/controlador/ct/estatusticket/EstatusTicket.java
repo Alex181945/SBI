beae3863a308511766337b60bc7eac19dae01181
Alex181945 <alestra55@hotmail.com>
2018-05-18 17:55:43 -0500

    Se inicia el controlador para estatus de ticket

--- sbi/src/main/java/com/senado/sbi/controlador/ct/estatusticket/EstatusTicket.java
+++ sbi/src/main/java/com/senado/sbi/controlador/ct/estatusticket/EstatusTicket.java
@@ -1,7 +1,20 @@
 package com.senado.sbi.controlador.ct.estatusticket;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.senado.sbi.configuracion.MensajeExito;
+import com.senado.sbi.configuracion.Vistas;
+import com.senado.sbi.modelo.ct.EstatusTicketM;
+import com.senado.sbi.modelo.datos.consulta.DosParametrosEnteros;
+import com.senado.sbi.modelo.seg.login.ULogin;
+import com.senado.sbi.rest.ct.EstatusTicketRest;
 
 /**
  * 
@@ -20,6 +33,63 @@ import org.springframework.web.bind.annotation.SessionAttributes;
 @SessionAttributes("Usuario")
 public class EstatusTicket {
 	
+	@Autowired
+	private EstatusTicketRest estatusTicketRest;
 	
+	@GetMapping(Vistas.CT_ESTATUS_TICKET_CONSULTA_R)
+	public ModelAndView consultaTodos(@ModelAttribute("Usuario") ULogin sessionUsu) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		mav.setViewName(Vistas.getCtEstatusTicketConsulta());
+		mav.addObject("", estatusTicketRest.consultaEstatusTicketes(2, sessionUsu.getcToken()));
+		
+		return mav;
+	}
+	
+	@GetMapping(Vistas.CT_ESTATUS_TICKET_FORMULARIO_R)
+	public ModelAndView formulario(@ModelAttribute("Usuario") ULogin sessionUsu) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+	}
+	
+	@PostMapping(Vistas.CT_ESTATUS_TICKET_INSERTA_R)
+	public ModelAndView inserta(@ModelAttribute("Usuario") ULogin sessionUsu,
+			@ModelAttribute("objEstatusTicket") EstatusTicketM objEstatusTicketM) {
+		
+		ModelAndView mav = new ModelAndView();
+
+		return mav;
+	}
+
+	@GetMapping(Vistas.CT_ESTATUS_TICKET_CONSULTA_UNO_R)
+	public ModelAndView consultaUno(@ModelAttribute("Usuario") ULogin sessionUsu,
+			@ModelAttribute("iIDEdificio") Integer iIDEdificio) {
+		
+		ModelAndView mav = new ModelAndView();
+
+		return mav;
+	}
+	
+	@PostMapping(Vistas.CT_ESTATUS_TICKET_EDITA_R)
+	public ModelAndView edita(@ModelAttribute("Usuario") ULogin sessionUsu,
+			@ModelAttribute("objEstatusTicket") EstatusTicketM objEstatusTicketM, 
+			@ModelAttribute("activo") String lActivo) {
+		
+		ModelAndView mav = new ModelAndView();
+		
+		return mav;
+		
+	}
+
+	@PostMapping(Vistas.CT_ESTATUS_TICKET_BORRA_R)	
+	public @ResponseBody String borra(@ModelAttribute("Usuario") ULogin sessionUsu,
+			@ModelAttribute("ID") Integer iIDEdificio) {
+		
+		
+		return null;
+	}
 
 }
