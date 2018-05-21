56eff4441cfd63c5ff80427e1f05f3c67df157b8
Alex181945 <alestra55@hotmail.com>
2018-05-21 11:32:14 -0500

    Inserta y consulta un registro exitoso

--- sbi/src/main/java/com/senado/sbi/configuracion/MensajeExito.java
+++ sbi/src/main/java/com/senado/sbi/configuracion/MensajeExito.java
@@ -5,6 +5,8 @@ public class MensajeExito {
 	private final static String EXITO_CT_EDIFICIO_INSERTA = "Edificio creado exitosamente";
 	private final static String EXITO_CT_EDIFICIO_EDITA   = "Edificio actualizado exitosamente";
 	private final static String EXITO_OP_TICKET_INSERTA   = "Ticket creado exitosamente";
+	private final static String EXITO_CT_ESTATUSTICKETS_INSERTA = "Estatus del Ticket creado exitosamente";
+	private final static String EXITO_CT_ESTATUSTICKETS_EDITA   = "Estatus del Ticket actualizado exitosamente";
 
 	public static String getExitoCtEdificioInserta() {
 		return EXITO_CT_EDIFICIO_INSERTA;
@@ -16,6 +18,14 @@ public class MensajeExito {
 
 	public static String getExitoOpTicketInserta() {
 		return EXITO_OP_TICKET_INSERTA;
+	}
+
+	public static String getExitoCtEstatusticketsInserta() {
+		return EXITO_CT_ESTATUSTICKETS_INSERTA;
+	}
+
+	public static String getExitoCtEstatusticketsEdita() {
+		return EXITO_CT_ESTATUSTICKETS_EDITA;
 	}
 	
 }
