c459cd626fd111cbf42a637d94cc71a3a4591cea
JenniHdez <hernandez.flores.jennifer@gmail.com>
2018-06-01 02:25:57 -0500

    Tengo duda, como yo tengo dos tablas de Resguardo y Eleemtno resgardo como lo cies a ami porque

---
+++ sbi/src/main/java/com/senado/sbi/modelo/op/ServAdmon/ServAdmonM.java
@@ -0,0 +1,111 @@
+package com.senado.sbi.modelo.op.ServAdmon;
+
+public class ServAdmonM {
+	private Integer iIDServAdministrados;
+	private Integer iIDEdificio;
+	private String  cPiso;
+	private String  cOficina;
+	private Integer iIDArea;
+	private Integer iIDSubArea;
+	private String  cResguardante;
+	private String  cResponsable;
+	private String  cFactura;
+	private String  cObs;
+	private String  cTipoRecibo; // Este campo tiene como id el iIDTipoServicio
+	private String  dtCreado;
+	private String  lActivo;
+	private Integer iIDCreaTicket;
+	private String  cUsuario;
+	
+	public Integer getiIDServAdministrados() {
+		return iIDServAdministrados;
+	}
+	public void setiIDServAdministrados(Integer iIDServAdministrados) {
+		this.iIDServAdministrados = iIDServAdministrados;
+	}
+	public Integer getiIDEdificio() {
+		return iIDEdificio;
+	}
+	public void setiIDEdificio(Integer iIDEdificio) {
+		this.iIDEdificio = iIDEdificio;
+	}
+	public String getcPiso() {
+		return cPiso;
+	}
+	public void setcPiso(String cPiso) {
+		this.cPiso = cPiso;
+	}
+	public String getcOficina() {
+		return cOficina;
+	}
+	public void setcOficina(String cOficina) {
+		this.cOficina = cOficina;
+	}
+	public Integer getiIDArea() {
+		return iIDArea;
+	}
+	public void setiIDArea(Integer iIDArea) {
+		this.iIDArea = iIDArea;
+	}
+	public Integer getiIDSubArea() {
+		return iIDSubArea;
+	}
+	public void setiIDSubArea(Integer iIDSubArea) {
+		this.iIDSubArea = iIDSubArea;
+	}
+	public String getcResguardante() {
+		return cResguardante;
+	}
+	public void setcResguardante(String cResguardante) {
+		this.cResguardante = cResguardante;
+	}
+	public String getcResponsable() {
+		return cResponsable;
+	}
+	public void setcResponsable(String cResponsable) {
+		this.cResponsable = cResponsable;
+	}
+	public String getcFactura() {
+		return cFactura;
+	}
+	public void setcFactura(String cFactura) {
+		this.cFactura = cFactura;
+	}
+	public String getcObs() {
+		return cObs;
+	}
+	public void setcObs(String cObs) {
+		this.cObs = cObs;
+	}
+	public String getcTipoRecibo() {
+		return cTipoRecibo;
+	}
+	public void setcTipoRecibo(String cTipoRecibo) {
+		this.cTipoRecibo = cTipoRecibo;
+	}
+	public String getDtCreado() {
+		return dtCreado;
+	}
+	public void setDtCreado(String dtCreado) {
+		this.dtCreado = dtCreado;
+	}
+	public String getlActivo() {
+		return lActivo;
+	}
+	public void setlActivo(String lActivo) {
+		this.lActivo = lActivo;
+	}
+	public Integer getiIDCreaTicket() {
+		return iIDCreaTicket;
+	}
+	public void setiIDCreaTicket(Integer iIDCreaTicket) {
+		this.iIDCreaTicket = iIDCreaTicket;
+	}
+	public String getcUsuario() {
+		return cUsuario;
+	}
+	public void setcUsuario(String cUsuario) {
+		this.cUsuario = cUsuario;
+	}
+	
+}
---
+++ sbi/src/main/resources/static/assets/js/funciones/deshabilitar.js
@@ -0,0 +1,11 @@
+function Deshabilitar() {
+		//Ingresamos un mensaje a mostrar
+			var mensaje = confirm("¿Deseas deshabilitar el resguardo?");
+			if (mensaje) {
+					alert("¡Aceptar!");
+
+			else {
+					alert("¡Cancelar!");
+				 }
+			}
+		}
